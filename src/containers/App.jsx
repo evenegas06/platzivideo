abe12a113f7389fb8108f03a6fb2f08ab865d07f
Eduardo Venegas Rivera <evenegas06@hotmail.com>
2021-05-14 01:23:21 -0500

    Ciustom Hooks

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -5,40 +5,33 @@ import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
+import useInitialState from '../hooks/useInitialState';
 
 import '../assets/styles/App.scss';
 
+const API = 'http://localhost:3000/initialState';
+
 const App = () => {
-
-    const [videos, setVideos] = useState([]);
-
-    useEffect( () => {
-        fetch('http://localhost:3000/initialState')
-            .then(response => response.json())
-            .then(data => setVideos(data));
-    }, []);
-
-    console.log(videos);
-
-    return (
+    //console.log(videos);
+    const initialState = useInitialState(API);
+    return initialState.length === 0 ? <h1>Loading...</h1> : (
         <div className="App">
             <Header />
             <Search />
+            {initialState.mylist.length > 0 &&
+                <Categories title="Mi lista">
+                    <Carousel>
+                        <CarouselItem />
+                    </Carousel>
+                </Categories>
+            }
             
-            <Categories title="Mi lista">
-                <Carousel>
-                    <CarouselItem />
-                    <CarouselItem />
-                    <CarouselItem />
-                    <CarouselItem />
-                </Carousel>
-            </Categories>
 
             <Categories title="Tendencias">
                 <Carousel>
-                    <CarouselItem />
-                    <CarouselItem />
-                    <CarouselItem />
+                    {initialState.trends.map(item =>
+                        <CarouselItem key={item.id} {...item}/>
+                    )}
                 </Carousel>
             </Categories>
 
