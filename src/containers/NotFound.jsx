1f409e15caa8d0c13dae12197f8bbd300850423f
Eduardo Venegas Rivera <evenegas06@hotmail.com>
2021-05-18 01:38:36 -0500

    Error 404 v1

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../assets/styles/components/NotFound.scss';
+
+
+const NotFound = () => {
+    return (
+        <React.Fragment>
+            <h1 className="not">Página no encontrada...</h1>
+        </React.Fragment>
+    );
+};
+
+export default NotFound;
