92f2df5b940465cb696a2cb735bec4a1aeb52d03
Eduardo Venegas Rivera <evenegas06@hotmail.com>
2021-05-25 01:33:29 -0500

    Correción ruta home en componente 404

--- src/containers/NotFound.jsx
+++ src/containers/NotFound.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Astronauta from '../assets/static/astronauta.svg';
+import { Link } from 'react-router-dom';
 
 import '../assets/styles/components/NotFound.scss';
 
@@ -9,7 +10,9 @@ const NotFound = () => {
             <div className="notFound">
                 <img className="slidein" src={Astronauta} alt="" />
                 <h2>Lo sentimos. Página no encontrada.</h2>
-                <a href="#">Volver al Home</a>
+                <Link to="/">
+                    Volver al Home
+                </Link>
             </div>
         </div>
     );
